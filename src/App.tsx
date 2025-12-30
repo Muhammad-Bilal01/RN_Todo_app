a468d9b386553f816674be389334eb858e31742d
Muhammad Bilal <mbilal254.m@gmail.com>
2025-12-31 03:01:55 +0500

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App = () => {
-  return <HomeScreen />;
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
 };
 
 export default App;
